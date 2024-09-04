6595abb8e730bdadc9fc67bc646f71a5ce453f41
Hosannaaaa <hosannabilewu@gmail.com>
2024-09-04 12:42:38 +0100

    Functions

---
+++ main.py
@@ -0,0 +1,24 @@
+# function is a set of instructions grouped together
+# function declaration is creating a function
+# function invocation is using a function
+def name(first_name, last_name):
+    print(first_name)
+    print(last_name)
+
+name("Hosanna", "Bilewu")
+name("Dami","Oluwasegun")
+
+def div(a,b):
+    return a/b
+def multiplication(c,d):
+    return c*d
+def sub(e,f):
+    return e-f
+def add(g,h):
+    return g+h
+
+print(div(36,4))
+print(multiplication(3,7))
+print(sub(10,5))
+print(add(23,16))
+    
